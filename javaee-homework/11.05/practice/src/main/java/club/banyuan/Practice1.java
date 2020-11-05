2b390a31d25fc98d4acd85b214c520b88c58b116
jianghao <1429346748@qq.com>
2020-11-05 19:54:34 +0800

    javaee

---
+++ javaee-homework/11.05/practice/src/main/java/club/banyuan/Practice1.java
@@ -0,0 +1,13 @@
+package club.banyuan;
+
+/**
+ * Hello world!
+ *
+ */
+public class Practice1
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
